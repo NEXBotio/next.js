4a7ad624b185870443f3153e8a2e1f19f05eebd3
OJ Kwon <1210596+kwonoj@users.noreply.github.com.>
2024-01-10 13:13:10 -0800

    wip

---
+++ packages/next-swc/crates/next-core/src/next_shared/transforms/next_page_config.rs
@@ -0,0 +1,19 @@
+use anyhow::Result;
+use async_trait::async_trait;
+use turbopack_binding::{
+    swc::core::{common::util::take::Take, ecma::ast::*},
+    turbopack::ecmascript::{CustomTransformer, EcmascriptInputTransform, TransformContext},
+};
+
+#[derive(Debug)]
+struct NextPageConfig {}
+
+#[async_trait]
+impl CustomTransformer for NextPageConfig {
+    async fn transform(&self, program: &mut Program, _ctx: &TransformContext<'_>) -> Result<()> {
+        let p = std::mem::replace(program, Program::Module(Module::dummy()));
+
+        *program = p;
+        Ok(())
+    }
+}
